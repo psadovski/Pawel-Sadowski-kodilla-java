35265680648ceb0ba5da5b2bcb35cf6f98203eff
psadovski <pawelsadowski92@gmail.com>
2018-07-04 23:51:22 +0200

    task6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        Integer result1 = calculator.add(5, 5);
+        Integer result2 = calculator.substract(5, 3);
+
+        if (result1.equals(10)){
+            System.out.println("Adding test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (result2.equals(2)){
+            System.out.println("Substracting test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public Integer add(int a, int b) {
+        return a + b;
+    }
+
+    public Integer substract(int a, int b) {
+        return a - b;
+    }
+}
