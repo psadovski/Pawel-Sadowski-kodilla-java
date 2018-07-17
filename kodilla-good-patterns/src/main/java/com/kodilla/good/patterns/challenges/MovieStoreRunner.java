f51b25b74acb7d9bdce8dfd27666a254affa4333
psadovski <39880148+psadovski@users.noreply.github.com>
2018-07-17 22:34:06 +0200

    task9.1 (#8)
    * task9.1
    
    * task9.1 | separator jako stała
    
    * task9.1 | usunięte entry i get Values

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreRunner.java
@@ -0,0 +1,23 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.kodilla.good.patterns.challenges.MovieStore.getMovies;
+
+public class MovieStoreRunner {
+
+    public static void main(String[] args) {
+        Map<String, List<String>> storeMap = new HashMap<>(getMovies());
+        final String SEPARATOR = " ! ";
+
+        String collect = storeMap.values()
+                .stream()
+                .flatMap(v -> v.stream())
+                .collect(Collectors.joining(SEPARATOR));
+
+        System.out.println(collect);
+    }
+}
