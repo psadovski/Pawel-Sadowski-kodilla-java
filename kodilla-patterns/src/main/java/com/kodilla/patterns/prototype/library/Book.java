c76a873ff7bc8ec930063a5de12c30badb04b143
psadovski <pawelsadowski92@gmail.com>
2018-07-28 12:19:43 +0200

    task11.2

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Book.java
@@ -0,0 +1,36 @@
+package com.kodilla.patterns.prototype.library;
+
+import java.time.LocalDate;
+
+public final class Book {
+    final String title;
+    final String author;
+    final LocalDate publicationDate;
+
+    public Book(final String title, final String author, final LocalDate publicationDate) {
+        this.title = title;
+        this.author = author;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public LocalDate getPublicationDate() {
+        return this.publicationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title ='" + title + '\'' +
+                ", author ='" + author + '\'' +
+                ", publicationDate =" + publicationDate +
+                '}';
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,44 @@
+package com.kodilla.patterns.prototype.library;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+public class LibraryTestSuite {
+    @Test
+    public void testGetBooks() {
+        //Given
+        //When
+        Library library = new Library("Book library 1");
+        IntStream.iterate(1,n -> n+1)
+                .limit(10)
+                .forEach(n -> library.getBooks().add(new Book("\"First step in Java \" part: " + n, "Tom", LocalDate.of(2008 + n, 06,7))));
+
+        Library clonedLibrary = null;
+        try {
+            clonedLibrary = library.shallowCopy();
+            clonedLibrary.setName("Book library 2");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+            deepClonedLibrary.setName("Book library 3");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        System.out.println(library);
+        System.out.println(clonedLibrary);
+        System.out.println(deepClonedLibrary);
+
+        //Then
+        Assert.assertEquals(10, library.getBooks().size());
+        Assert.assertEquals(10, clonedLibrary.getBooks().size());
+        Assert.assertEquals(10, deepClonedLibrary.getBooks().size());
+    }
+}
