0af2974d970a9cd5a8afa7dbc67ea5d04accddae
psadovski <pawelsadowski92@gmail.com>
2018-08-03 11:05:41 +0200

    task11.1 | po poprawie

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -5,18 +5,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LoggerTestSuite {
-    @BeforeClass
-    public static void logInProcessOperations() {
-        Logger.getInstance().log("Sending e-mail");
-    }
-
     @Test
     public void testGetLastLog() {
         //Given
-        //When
-        String lastLog = Logger.getInstance().getLastLog();
-        System.out.println("Last log: " + lastLog);
-        //Then
-        Assert.assertEquals("Sending e-mail", lastLog);
+        Logger.getInstance().log("Sending e-mail");
+        //When & Then
+        Assert.assertEquals("Sending e-mail", Logger.getInstance().getLastLog());
     }
 }
