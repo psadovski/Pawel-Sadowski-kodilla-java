5aa036fe54142a855b3e5fc66e6bddb3b0e6b2a4
psadovski <pawelsadowski92@gmail.com>
2018-08-28 21:55:03 +0200

    task7.7 | podczas poprawy

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsMoveComparator.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsMoveComparator.java
@@ -1,44 +1,46 @@
 package com.kodilla.rps;
 
 public class RpsMoveComparator {
-    public static RpsRoundResult result;
-
     public static RpsRoundResult compare(RpsTurn userTurn, RpsTurn computerTurn) {
         switch (userTurn) {
             case ROCK:
                 if (computerTurn == RpsTurn.ROCK) {
-                    return result = RpsRoundResult.DRAW;
+                    return RpsRoundResult.DRAW;
                 }
                 if (computerTurn == RpsTurn.SCISSORS) {
-                    return result = RpsRoundResult.USER;
+                    return RpsRoundResult.USER;
                 }
                 if (computerTurn == RpsTurn.PAPER) {
-                    return result = RpsRoundResult.COMPUTER;
+                    return RpsRoundResult.COMPUTER;
                 }
-                break;
             case PAPER:
                 if (computerTurn == RpsTurn.ROCK) {
-                    return result = RpsRoundResult.USER;
+                    return RpsRoundResult.USER;
                 }
                 if (computerTurn == RpsTurn.SCISSORS) {
-                    return result = RpsRoundResult.COMPUTER;
+                    return RpsRoundResult.COMPUTER;
                 }
                 if (computerTurn == RpsTurn.PAPER) {
-                    return result = RpsRoundResult.DRAW;
+                    return RpsRoundResult.DRAW;
                 }
-                break;
             case SCISSORS:
                 if (computerTurn == RpsTurn.ROCK) {
-                    return result = RpsRoundResult.COMPUTER;
+                    return RpsRoundResult.COMPUTER;
                 }
                 if (computerTurn == RpsTurn.SCISSORS) {
-                    return result = RpsRoundResult.DRAW;
+                    return RpsRoundResult.DRAW;
                 }
                 if (computerTurn == RpsTurn.PAPER) {
-                    return result = RpsRoundResult.USER;
+                    return RpsRoundResult.USER;
                 }
-                break;
+            case END:
+                return RpsRoundResult.END;
+
+            case REPLAY:
+                return RpsRoundResult.REPLAY;
+
+            default:
+                return RpsRoundResult.BAD;
         }
-        return result;
     }
 }
