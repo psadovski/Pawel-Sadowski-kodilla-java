1678cf4b5b537b609844ac162bc149b236558fb7
psadovski <pawelsadowski92@gmail.com>
2018-10-02 10:26:16 +0200

    task20.4 | poprawione formatowanie kodu

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Mentor.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Mentor.java
@@ -1,24 +1,31 @@
 package com.kodilla.patterns2.observer.homework;
 
 public class Mentor implements Observer {
+
     private static final String MSG = "%s: New tasks in the queue: %s (total: %s %s)";
+
     private final String name;
     private int taskCount;
+
     public Mentor(String name) {
         this.name = name;
     }
+
     @Override
     public void update(StudentTaskQueue taskQueue) {
         System.out.println(String.format(MSG, name, taskQueue.getTaskQueueName(),
                 taskQueue.getTasks().size(), task(taskQueue.getTasks().size())));
         taskCount++;
     }
+
     public String getUsername() {
         return name;
     }
+
     public int getUpdateCount() {
         return taskCount;
     }
+    
     private String task(long size) {
         return size == 1 ? "task" : "tasks";
     }
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/MentorTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/MentorTestSuite.java
@@ -2,6 +2,7 @@ package com.kodilla.patterns2.observer.homework;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
 public class MentorTestSuite {
     @Test
     public void testUpdate() {
